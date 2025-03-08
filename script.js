73afe3e685159e1a36abcd31cc6e2e4aa2b92a36
OfirTzrik <ofirc36@gmail.com>
2025-03-08 18:06:16 +0200

    Switch playRound for better readability
    Switched the if statements in playRound with switch statements for
    better readability. Also added comments

--- script.js
+++ script.js
@@ -34,36 +34,55 @@ function playGame() {
         Play a single round of Rock, Paper, Scissors
     */
     function playRound(computerChoice, playerChoice) {
-        if(computerChoice === "rock") {
-            if(playerChoice === "rock") {
-                console.log("It's a draw!");
-            } else if(playerChoice === "paper") {
-                console.log("You win! Paper beats rock.");
-                playerScore++;
-            } else {
-                console.log("You lose! Rock beats scissors.");
-                computerScore++;
-            }
-        } else if(computerChoice === "paper") {
-            if(playerChoice === "rock") {
-                console.log("You lose! Paper beats rock.");
-                computerScore++;
-            } else if(playerChoice === "paper") {
-                console.log("It's a draw!");
-            } else {
-                console.log("You win! Scissors beats paper.");
-                playerScore++;
-            }
-        } else {
-            if(playerChoice === "rock") {
-                console.log("You win! Rock beats scissors.");
-                playerScore++;
-            } else if(playerChoice === "paper") {
-                console.log("You lose! Scissors beats paper.");
-                computerScore++;
-            } else {
-                console.log("It's a draw!");
-            }
+        switch(computerChoice) {
+            // Computer chose rock
+            case "rock":
+                switch(playerChoice) {
+                    case "rock":
+                        console.log("It's a draw!");
+                        break;
+                    case "paper":
+                        console.log("You win! Paper beats rock.");
+                        playerScore++;
+                        break;
+                    case "scissors":
+                        console.log("You lose! Rock beats scissors.");
+                        computerScore;
+                        break;
+                }
+                break;
+            // Computer chose paper
+            case "paper":
+                switch(playerChoice) {
+                    case "rock":
+                        console.log("You lose! Paper beats rock.");
+                        computerScore++;
+                        break;
+                    case "paper":
+                        console.log("It's a draw!");
+                        break;
+                    case "scissors":
+                        console.log("You win! Scissors beats paper.");
+                        playerScore++;
+                        break;
+                }
+                break;
+            // Computer chose scissors
+            case "scissors":
+                switch(playerChoice) {
+                    case "rock":
+                        console.log("You win! Rock beats scissors.");
+                        playerScore++;
+                        break;
+                    case "paper":
+                        console.log("You lose! Scissors beats paper.");
+                        computerScore++;
+                        break;
+                    case "scissors":
+                        console.log("It's a draw!");
+                        break;
+                }
+                break;
         }
     }
 
